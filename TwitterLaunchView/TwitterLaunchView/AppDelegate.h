7022003105e01c6eba4013ead39940c8c4aa2f34
cxz <1604101058@qq.com>
2017-07-09 15:06:37 +0800

    初始化项目

---
+++ TwitterLaunchView/TwitterLaunchView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterLaunchView
+//
+//  Created by TouchWorld on 2017/7/9.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterLaunchView/TwitterLaunchView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterLaunchView
+//
+//  Created by TouchWorld on 2017/7/9.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
